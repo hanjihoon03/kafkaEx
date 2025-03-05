fea5cb689181fbcb783b0aac52613b4cb9fe4048
hanjihoon03 <gkswlgns7653@gmail.com>
2025-03-05 22:37:21 +0900

    producer 설정

---
+++ producer/src/main/java/com/kafka_sample/producer/ProducerService.java
@@ -0,0 +1,24 @@
+package com.kafka_sample.producer;
+
+/**
+ * @author : hanjihoon
+ * @Date : 2025. 03. 05.
+ */
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProducerService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+
+    public void sendMessage(String topic , String key, String message) {
+        for (int i = 0; i < 10; i++) {
+            kafkaTemplate.send(topic, key, message + " " + i);
+        }
+
+    }
+}
